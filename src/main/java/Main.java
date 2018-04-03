360df6f3708fb52f834f929e8f4f92a59f65e5cd
Zykai <35534281+Zykai@users.noreply.github.com>
2018-04-03 14:41:33 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,99 +1,75 @@
-import java.nio.FloatBuffer;
+import org.joml.Vector4f;
 
-import org.lwjgl.BufferUtils;
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.opengl.*;
-
-import Shaders.Shader;
+import UserInterface.Field;
+import UserInterface.FieldBox;
+import UserInterface.MouseListener;
 
 public class Main {
 	
-	private static Shader shader;
-	private static int positionVbo, colorVbo, vao;
-
-	public static void createObjects() {
-		// Erstellen der VBO
-		positionVbo = GL15.glGenBuffers();
-		colorVbo = GL15.glGenBuffers();
-		
-		
-		// Einfügen der Positionsdaten in einen Buffer
-		float[] positionData = {-0.5f, -0.5f,
-			     0.5f, -0.5f, 
-			     0.0f,  0.50f};
-		FloatBuffer positionBuffer = BufferUtils.createFloatBuffer(positionData.length);
-		positionBuffer.put(positionData);
-		positionBuffer.flip();
-		
-		// Einfügen der Farben in einen Buffer
-		float[] colorData = {
-			1, 0, 0,
-			0, 1, 0,
-			0, 0, 1
-		};
-		FloatBuffer colorBuffer = BufferUtils.createFloatBuffer(colorData.length);
-		colorBuffer.put(colorData);
-		colorBuffer.flip();
-		
-		
-		// Füllen der VBO mit Werten aus dem Buffer
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, positionVbo	);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, positionBuffer, GL15.GL_STATIC_DRAW);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, colorVbo	);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, colorBuffer, GL15.GL_STATIC_DRAW);
-		
-		// Erstellen des VAO
-		vao = GL30.glGenVertexArrays();
-		GL30.glBindVertexArray(vao);
-		
-		// Bindet VBO an VBO
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, positionVbo);
-		GL20.glVertexAttribPointer(0, 2, GL11.GL_FLOAT, false, 0, 0);	
-		GL20.glEnableVertexAttribArray(0);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, colorVbo);
-		GL20.glVertexAttribPointer(1, 3, GL11.GL_FLOAT, false, 0, 0);	
-		GL20.glEnableVertexAttribArray(1);
-		
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-		GL30.glBindVertexArray(0);
-	}
-	
-	public static void loadShader() {
-		shader = new Shader("src/main/java/Shaders/vertexShader", "src/main/java/Shaders/fragmentShader");
-		shader.use();
-	}
-	
-	// Räumt Ressourcen nach Programmende auf
-	public static void cleanUp() {
-		shader.delete();
-		GL30.glDeleteVertexArrays(vao);
-		GL15.glDeleteBuffers(positionVbo);
-		GL15.glDeleteBuffers(colorVbo);
-	}
 	
 	public static void main(String[] args) {
 		
 		Window w = new Window(800, 600, false);
 		w.showWindow();
+		w.enableBlend();
 		
 		
-		loadShader();
-		createObjects();
 		
-		GL30.glBindVertexArray(vao);
 		
-		float height = 0;
 		
+		// Elemente mit Event-Listener
+		Field.init(800, 600);
+		final Field f = new Field(300, 100, 50, 50, new Vector4f(1, 0, 0, 0.5f));
+		f.setMouseListener(new MouseListener() {
+			@Override
+			public void mouseClick() {
+				
+			}
+			
+			@Override 
+			public void mouseEnter() {
+				f.setColor(new Vector4f(1, 0, 0, 0.8f));
+				f.increaseSizeScaled(10, 10);
+			}
+			
+			@Override 
+			public void mouseLeave() {
+				f.setColor(new Vector4f(1, 0, 0, 0.5f));
+				f.increaseSizeScaled(-10, -10);
+			}
+		});
+		Field f2 = new Field(300, 100, 50, 200, new Vector4f(0,0,1, 0.5f));
+		f2.setMouseListener(new MouseListener() {
+			@Override
+			public void mouseClick() {
+				
+			}
+			
+			@Override 
+			public void mouseEnter() {
+				f2.setColor(new Vector4f(0,0,1, 0.8f));
+				f2.increaseSizeScaled(10, 10);
+			}
+			
+			@Override 
+			public void mouseLeave() {
+				f2.setColor(new Vector4f(0,0,1, 0.5f));
+				f2.increaseSizeScaled(-10, -10);
+			}
+		});
+		
+		FieldBox box = new FieldBox(400, 350, 200, 150, new Vector4f(0.0f, 0.0f, 0.0f, 0.2f));
+		box.addField(f);
+		box.addField(f2);
 		// Main-Loop
 		while(!w.shouldClose()) {
 			w.clear();
-			height = (float) Math.sin(GLFW.glfwGetTime());
-			shader.setFloat("height", height);
-			GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 3);
+			
+			box.draw();
+			box.processInput(Globals.xMousePosition, Globals.yMousePosition, Globals.newClicked);
 			w.updateWindow();
 		}
-		
-		cleanUp();
+		Field.cleanUp();
 		
 		w.closeWindow();		
 
